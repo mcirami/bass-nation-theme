34d06e36c736a1ef957187d877433b5e546f4acd
Matteo Cirami <mcirami@users.noreply.github.com>
2024-11-01 16:32:09 -0500

    stylig forums

--- bbpress/loop-single-reply.php
+++ bbpress/loop-single-reply.php
@@ -9,34 +9,35 @@
 
 ?>
 
-<div id="post-<?php bbp_reply_id(); ?>" class="bbp-reply-header">
 
-	<div class="bbp-meta">
-
-		<?php if ( bbp_is_single_user_replies() ) : ?>
-
-			<span class="bbp-header">
-				<?php _e( 'in reply to: ', 'bbpress' ); ?>
-				<a class="bbp-topic-permalink" href="<?php bbp_topic_permalink( bbp_get_reply_topic_id() ); ?>"><?php bbp_topic_title( bbp_get_reply_topic_id() ); ?></a>
-			</span>
-
-		<?php endif; ?>
-
-		<a href="<?php bbp_reply_url(); ?>" class="bbp-reply-permalink">#<?php bbp_reply_id(); ?></a>
-
-		<?php do_action( 'bbp_theme_before_reply_admin_links' ); ?>
-
-		<?php bbp_reply_admin_links(); ?>
-
-		<?php do_action( 'bbp_theme_after_reply_admin_links' ); ?>
-
-	</div><!-- .bbp-meta -->
-
-</div>
 <!-- #post-<?php bbp_reply_id(); ?> -->
 
 <div <?php bbp_reply_class(); ?>>
 
+	<div id="post-<?php bbp_reply_id(); ?>" class="bbp-reply-header">
+
+		<div class="bbp-meta">
+
+			<?php if ( bbp_is_single_user_replies() ) : ?>
+
+				<span class="bbp-header">
+					<?php _e( 'in reply to: ', 'bbpress' ); ?>
+					<a class="bbp-topic-permalink" href="<?php bbp_topic_permalink( bbp_get_reply_topic_id() ); ?>"><?php bbp_topic_title( bbp_get_reply_topic_id() ); ?></a>
+				</span>
+
+			<?php endif; ?>
+
+			<a href="<?php bbp_reply_url(); ?>" class="bbp-reply-permalink">#<?php bbp_reply_id(); ?></a>
+
+			<?php do_action( 'bbp_theme_before_reply_admin_links' ); ?>
+
+			<?php bbp_reply_admin_links(); ?>
+
+			<?php do_action( 'bbp_theme_after_reply_admin_links' ); ?>
+
+		</div><!-- .bbp-meta -->
+
+	</div>
 	<div class="bbp-reply-author">
 
 		<?php do_action( 'bbp_theme_before_reply_author_details' ); ?>
